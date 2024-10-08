2ec7a660a28ba2e38c90c47ae7a34377a136cf1e
ira_koch <irakoch228@gmail.com>
2024-10-08 21:18:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
+import {useTelegram} from "./hooks/useTelegram";
+
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
      tg.ready();
     }, [])
 
 
-    const onClose = () => {
-        tg.close()
-
-
-    }
   return (
     <div className="App">
-      ira_samaya_krasivaya
-        <button> onClick = {onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
+
     </div>
   );
 }
