61a0392a19c9de392eac0659be0eb02e0150e1df
ira_koch <irakoch228@gmail.com>
2024-10-09 21:24:29 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 
 function App() {
@@ -17,7 +19,11 @@ function App() {
   return (
     <div className="App">
         <Header />
-      <button onClick={onToggleButton}>toggle</button>
+        <Routes>
+            <Route index element={<ProductList />}/>
+            <Route path={'Form'} element={<Form />}/>
+        </Routes>
+
 
     </div>
   );
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Form.css';
+
+const Form = () => {
+
+    return (
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+            />
+            <select className={'select'}>
+                <option value={'physical'}>Физ.лицо</option>
+                <option value={'legal'}>Юр.лицо</option>
+            </select>
+
+        </div>
+    );
+};
+
+export default Form;
