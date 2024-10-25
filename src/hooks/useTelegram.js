2498041226598f547dcbe9c46070b7abd7c2513d
ira_koch <irakoch228@gmail.com>
2024-10-25 19:26:10 +0300

    init

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.WebApp;
 
-export function useTelegram(){
+export function useTelegram() {
 
 
     const onClose = () => {
@@ -19,5 +19,6 @@ export function useTelegram(){
         onToggleButton,
          tg,
         user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     }
 }
