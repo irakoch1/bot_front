31ea2ebf80721afcc2f8f1cb425800d392cd1e64
ira_koch <irakoch228@gmail.com>
2024-10-29 23:36:49 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-       fetch('http://localhost:8000/web-data', {
+       fetch('http://45.89.189.79:8000/web-data', {
            method: 'POST',
            headers: {
                'Content-Type': 'application/json',
